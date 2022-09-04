70ecc5cd0e8963b7a2538dd242a4252826a12f95
cirorey20 <cirorey95@gmail.com>
2022-09-04 20:27:35 -0300

    feat(db/models Profile): add model Profile

---
+++ api/src/db/models/profile.model.js
@@ -0,0 +1,90 @@
+const { Model, DataTypes, Sequelize } = require('sequelize');
+
+const PROFILE_TABLE = 'profiles';
+
+const ProfileSchema = {
+  id: {
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+    type: DataTypes.INTEGER
+  },
+  headline: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    defaultValue: 'FullStackDevelper'
+  },
+  about: {
+    allowNull: false,
+    type: DataTypes.TEXT,
+    defaultValue: 'About'
+  },
+  experience: {
+    allowNull: false,
+    type: DataTypes.TEXT,
+    defaultValue: 'Experience'
+  },
+  skillstech: {
+    allowNull: true,
+    type: DataTypes.JSON,
+    defaultValue: ['Js', 'Php']
+  },
+  skillsnotech: {
+    allowNull: true,
+    type: DataTypes.JSON,
+    defaultValue: ['Comunication', 'Lider']
+  },
+  age: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    defaultValue: 'age'
+  },
+  img: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    defaultValue: 'https://avatars.githubusercontent.com/u/74796071?v=4'
+  },
+  github: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    defaultValue: 'myGitHub.com'
+  },
+  linkedin: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    defaultValue: 'myLinkdedin.com'
+  },
+  email: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    defaultValue: 'myemail@gmail.com'
+  },
+  createdAt: {
+    allowNull: false,
+    type: DataTypes.DATE,
+    field: 'create_at',
+    defaultValue: Sequelize.NOW
+  }
+
+}
+
+class Profile extends Model {
+    static associate(models) {
+      //associations
+      
+    }
+    static config(sequelize) {
+      return {
+        sequelize,
+        tableName: PROFILE_TABLE,
+        modelName: 'Profile',
+        timestamps: false
+      }
+    }
+  }
+  
+  module.exports = {
+    PROFILE_TABLE,
+    ProfileSchema,
+    Profile
+  }
