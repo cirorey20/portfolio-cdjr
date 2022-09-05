70f4e1aab5abaa5921399a77ebac0eba57a2534c
cirorey20 <cirorey95@gmail.com>
2022-09-05 14:16:59 -0300

    (model/index Profile): add model Profile in setupModels

--- api/src/db/models/index.js
+++ api/src/db/models/index.js
@@ -1,9 +1,11 @@
 const { User, UserSchema } = require('./user.model.js');
 const { Post, PostSchema } = require('./post.model');
+const { Profile, ProfileSchema } = require('./profile.model');
 
 function setupModels(sequelize) {
     User.init(UserSchema, User.config(sequelize));
     Post.init(PostSchema, Post.config(sequelize));
+    Profile.init(ProfileSchema, Profile.config(sequelize));
 }
 
 module.exports = setupModels;
