1c35db6b94e0b8da8b37ce14b7be80092e012cd9
cirorey20 <cirorey95@gmail.com>
2022-09-07 02:33:15 -0300

    add procees.PORT in ./index

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const routerApi = require('./src/routers');
 const { logError, errorHandler } = require('./src/middlewares/error.handler.js');
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 
